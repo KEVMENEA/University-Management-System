8ddbfe8dd8704cdeb47d7ed41ed4651eebba29c0
ratanak <ratanakcoding.it@gmail.com>
2026-07-01 14:35:51 +0700

    new

--- src/main/java/com/universitymanagement/user/service/KeycloakToUser.java
+++
@@ -1,8 +0,0 @@
-package com.universitymanagement.user.service;
-
-import com.universitymanagement.user.entity.User;
-import org.keycloak.representations.idm.UserRepresentation;
-
-public interface KeycloakToUser {
-    User mapKeycloakToUser(UserRepresentation kcUser);
-}
